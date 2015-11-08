f88ab9f42e6e9a3787809b6074a9db13be87e83c
Aida Denisa Opîrlesc <Aida Denisa Oprlesc>
2015-11-08 10:32:38 +0200

    Upgraded landing page and adminisration view

--- frontend/src/administrare/administrare.controller.js
+++ frontend/src/administrare/administrare.controller.js
@@ -2,4 +2,8 @@ angular
 	.module('htmlapp')
 	.controller('AdministrareController', function (){
 		console.log('AdministrareController');
-	});
+	})
+	.module('witchMenu', []);
+    app.controller('switchMenu', ['$scope', function ($scope) {
+        $scope.tab = 'Mese';
+    }]);
